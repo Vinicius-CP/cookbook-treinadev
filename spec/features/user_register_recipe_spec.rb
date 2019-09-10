0079bbb1c9a3aa5510fa578001ad515307107762
Vinícius Cunha <vinicius_cunha2000@hotmail.com>
2019-09-10 20:18:39 -0300

    WIP erro de validação

--- spec/features/user_register_recipe_spec.rb
+++ spec/features/user_register_recipe_spec.rb
@@ -24,4 +24,20 @@ feature 'User register recipe' do
         expect(page).to have_css('p', text: 'Adicione tomate, alface, cebola, um pouco de sal e vinagre')
         expect(page).to have_content('Receita criada com sucesso!')   
     end
+
+    scenario 'and must fill in all fields' do
+        visit root_path
+        click_on 'Cadastrar receita'
+
+        fill_in 'Título', with: ''
+        fill_in 'Tipo da receita', with: 'Salada'
+        fill_in 'Cozinha', with: 'Brasileira'
+        fill_in 'Dificuldade', with: 'Fácil'
+        fill_in 'Tempo de preparo', with: '15'
+        fill_in 'Ingredientes', with: 'Tomate, alface, cebola e vinagre'
+        fill_in 'Método de preparo', with: 'Adicione tomate, alface, cebola, um pouco de sal e vinagre'
+        click_on 'Enviar'
+
+        expect(page).to have_content('Os campos devem ser preenchidos') 
+    end
 end 
