aed7041f014a772b0a36a9c504ba5f93b375f820
Vinícius Cunha <vinicius_cunha2000@hotmail.com>
2019-09-12 12:04:46 -0300

    Done Atualização do testes

--- spec/features/user_register_recipe_spec.rb
+++ spec/features/user_register_recipe_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 feature 'User register recipe' do
     scenario 'successfully' do
 
+        recipe_type = create(:recipe_type)
+
         visit root_path
         click_on 'Cadastrar receita'
 
         fill_in 'Título', with: 'Vinagrete'
-        fill_in 'Tipo de receita', with: 'Salada'
+        select 'Salada', from: 'Tipo de receita'
         fill_in 'Cozinha', with: 'Brasileira'
         fill_in 'Dificuldade', with: 'Fácil'
         fill_in 'Tempo de preparo', with: '15'
@@ -23,14 +25,16 @@ feature 'User register recipe' do
         expect(page).to have_css('p', text: 'Tomate, alface, cebola e vinagre')
         expect(page).to have_css('p', text: 'Adicione tomate, alface, cebola, um pouco de sal e vinagre')
         expect(page).to have_content('Receita criada com sucesso!')   
+        expect(Recipe.last.id).to eq (1)
     end
 
     scenario 'and must fill in all fields' do
+        recipe_type = create(:recipe_type)
         visit root_path
         click_on 'Cadastrar receita'
 
         fill_in 'Título', with: ''
-        fill_in 'Tipo de receita', with: 'Salada'
+        select 'Salada', from: 'Tipo de receita'
         fill_in 'Cozinha', with: 'Brasileira'
         fill_in 'Dificuldade', with: 'Fácil'
         fill_in 'Tempo de preparo', with: '15'
